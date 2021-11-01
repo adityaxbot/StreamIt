5107848f3d7695f74bf73408572d3ad3951e3ec6
Flux Inc <92753079+workforce-bot4917@users.noreply.github.com>
2021-11-01 16:42:18 +0530

    Add files via upload

---
+++ WebStreamer/bot/plugins/start.py
@@ -0,0 +1,198 @@
+from WebStreamer.bot import StreamBot
+from WebStreamer.vars import Var
+from WebStreamer.utils.human_readable import humanbytes
+from WebStreamer.utils.database import Database
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.errors import UserNotParticipant
+db = Database(Var.DATABASE_URL, Var.SESSION_NAME)
+
+
+@StreamBot.on_message(filters.command('start') & filters.private & ~filters.edited)
+async def start(b, m):
+    if not await db.is_user_exist(m.from_user.id):
+        await db.add_user(m.from_user.id)
+        await b.send_message(
+            Var.BIN_CHANNEL,
+            f"#NEW_USER: \n\nNew User [{m.from_user.first_name}](tg://user?id={m.from_user.id}) Started !!"
+        )
+    usr_cmd = m.text.split("_")[-1]
+    if usr_cmd == "/start":
+        if Var.UPDATES_CHANNEL is not None:
+            try:
+                user = await b.get_chat_member(Var.UPDATES_CHANNEL, m.chat.id)
+                if user.status == "kicked":
+                    await b.send_message(
+                        chat_id=m.chat.id,
+                        text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/fluxbots).",
+                        parse_mode="markdown",
+                        disable_web_page_preview=True
+                    )
+                    return
+            except UserNotParticipant:
+                await b.send_message(
+                    chat_id=m.chat.id,
+                    text="**Please Join My Updates Channel to use this Bot!**\n\nDue to Overload, Only Channel Subscribers can use the Bot!\n\n",
+                    reply_markup=InlineKeyboardMarkup(
+                        [
+                            [
+                                InlineKeyboardButton("🤖 Join Updates Channel", url=f"https://t.me/{Var.UPDATES_CHANNEL}")
+                            ]
+                        ]
+                    ),
+                    parse_mode="markdown"
+                )
+                return
+            except Exception:
+                await b.send_message(
+                    chat_id=m.chat.id,
+                    text="Something went Wrong. Contact my [Support Group](https://t.me/fluxsupport).",
+                    parse_mode="markdown",
+                    disable_web_page_preview=True)
+                return
+        await m.reply_text(
+            text="""👋 Hi, {}\n
+<b>✨ I'm A Telegram Bot 🤖 That Can Generate Permanent Download 📥 Links 🔗 For Provided Telegram File/Media.</b\n
+<b>Click /help For More Information Regarding Bot.</b>\n
+<b>🧑‍💻 Developer : </b>@rulebreakerzzz \n\n
+<b>⚡️ Channel : </b> @fluxbots \n\n
+<b>👮‍♂️ Support : </b>@fluxsupport \n """
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [InlineKeyboardButton(' Channel ', url='t.me/fluxbots'), InlineKeyboardButton(' Support ', url='t.me/fluxsupport')],
+                    [InlineKeyboardButton(' Developer ', url='t.me/rulebreakerzzz'), InlineKeyboardButton(' Source ', url='https://github.com/workforce-bot4917/StreamIt')]
+                ]
+            ),
+            disable_web_page_preview=True
+        )
+    else:
+        if Var.UPDATES_CHANNEL is not None:
+            try:
+                user = await b.get_chat_member(Var.UPDATES_CHANNEL, m.chat.id)
+                if user.status == "kicked":
+                    await b.send_message(
+                        chat_id=m.chat.id,
+                        text="Sorry Sir, You Are Not Allowed To Use Me. Contact My [Developer](t.me/rulebreakerzzz).",
+                        parse_mode="markdown",
+                        disable_web_page_preview=True
+                    )
+                    return
+            except UserNotParticipant:
+                await b.send_message(
+                    chat_id=m.chat.id,
+                    text="**Please Join My Updates Channel To Use This Bot!**\n\nDue to Overload, Only Channel Subscribers Can Use This Bot!\n\n © @rulebreakerzzz | @fluxbots",
+                    reply_markup=InlineKeyboardMarkup(
+                        [
+                            [
+                                InlineKeyboardButton("🤖 Join Updates Channel", url=f"t.me/{Var.UPDATES_CHANNEL}")
+                            ],
+                            [
+                                InlineKeyboardButton("🔄 Refresh / Try Again",
+                                                     url=f"t.me/streamer_fluxbot?start=Professor_{usr_cmd}")
+                            ]
+                        ]
+                    ),
+                    parse_mode="markdown"
+                )
+                return
+            except Exception:
+                await b.send_message(
+                    chat_id=m.chat.id,
+                    text="Something went Wrong. Contact my [Developer](t.me/rulebreakerzzz).",
+                    parse_mode="markdown",
+                    disable_web_page_preview=True)
+                return
+
+        get_msg = await b.get_messages(chat_id=Var.BIN_CHANNEL, message_ids=int(usr_cmd))
+
+        file_size = None
+        if get_msg.video:
+            file_size = f"{humanbytes(get_msg.video.file_size)}"
+        elif get_msg.document:
+            file_size = f"{humanbytes(get_msg.document.file_size)}"
+        elif get_msg.audio:
+            file_size = f"{humanbytes(get_msg.audio.file_size)}"
+
+        file_name = None
+        if get_msg.video:
+            file_name = f"{get_msg.video.file_name}"
+        elif get_msg.document:
+            file_name = f"{get_msg.document.file_name}"
+        elif get_msg.audio:
+            file_name = f"{get_msg.audio.file_name}"
+
+        stream_link = "https://{}/{}".format(Var.FQDN, get_msg.message_id) if Var.ON_HEROKU or Var.NO_PORT else \
+            "http://{}:{}/{}".format(Var.FQDN,
+                                     Var.PORT,
+                                     get_msg.message_id)
+
+        msg_text = """<u>Your Link Generated</u>\n
+<b>📂FILE NAME :</b> {}\n
+<b>📦FILE SIZE :</b> {}\n
+<b>🔗DOWNLOAD :</b> {}\n
+<b>⚠️NOTE : This Is Permanent Link.</b>\n
+© @rulebreakerzzz | @fluxbots """
+        await m.reply_text(
+            text=msg_text.format(file_name, file_size, stream_link),
+            parse_mode="Markdown",
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Download Link 🔗", url=stream_link)]])
+        )
+
+
+@StreamBot.on_message(filters.command('help') & filters.private & ~filters.edited)
+async def help_handler(bot, message):
+    if not await db.is_user_exist(message.from_user.id):
+        await db.add_user(message.from_user.id)
+        await bot.send_message(
+            Var.BIN_CHANNEL,
+            f"#NEW_USER: \n\nNew User [{message.from_user.first_name}](tg://user?id={message.from_user.id}) Started !!"
+        )
+    if Var.UPDATES_CHANNEL is not None:
+        try:
+            user = await bot.get_chat_member(Var.UPDATES_CHANNEL, message.chat.id)
+            if user.status == "kicked":
+                await bot.send_message(
+                    chat_id=message.chat.id,
+                    text="Sorry Sir, You are Banned to use me. Contact my [Developer](https://t.me/rulebreakerzzz).",
+                    parse_mode="markdown",
+                    disable_web_page_preview=True
+                )
+                return
+        except UserNotParticipant:
+            await bot.send_message(
+                chat_id=message.chat.id,
+                text="**Please Join My Updates Channel to use this Bot!**\n\nDue to Overload, Only Channel Subscribers can use the Bot!",
+                reply_markup=InlineKeyboardMarkup(
+                    [
+                        [
+                            InlineKeyboardButton("🤖 Join Updates Channel", url=f"https://t.me/{Var.UPDATES_CHANNEL}")
+                        ]
+                    ]
+                ),
+                parse_mode="markdown"
+            )
+            return
+        except Exception:
+            await bot.send_message(
+                chat_id=message.chat.id,
+                text="Something went Wrong. Contact my [Developer](https://t.me/rulebreakerzzz).",
+                parse_mode="markdown",
+                disable_web_page_preview=True)
+            return
+    await message.reply_text(
+        text="It's Not That Tough🎗\n\n
+             - Just Send Me Any Telegram File/Media.📂
+             - I Will Generate An External Download Link For Provided File.🔗
+             - Add Me In Groups/Channels For Direct Download Links.
+             - Permanent High-Speed Download Links Are Generated.
+             - Don't Send Porn Contents🔞.\n
+             <b>Contact Developer🧑‍💻 </b>: <a href='https://t.me/rulebreakerzzz'>亗 𝗣𝗥𝗢𝗙𝗘𝗦𝗦𝗢𝗥 亗</a>", 
+  parse_mode="Markdown",
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [InlineKeyboardButton(' Channel ', url='t.me/fluxbots'), InlineKeyboardButton(' Support ', url='t.me/fluxsupport')],
+                    [InlineKeyboardButton(' Developer ', url='t.me/rulebreakerzzz'), InlineKeyboardButton(' Source ', url='https://github.com/workforce-bot4917/StreamIt')]
+            ]
+        )
+    )
